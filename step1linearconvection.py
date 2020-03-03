718184103f1754452a1fb95e9f7c446709b13ce1
Felix Steinberger Eriksson <felix.s.eriksson@gmail.com>
2020-03-04 00:21:04 +0100

    added thus far progress on 12 steps to cfd program

---
+++ step1linearconvection.py
@@ -0,0 +1,30 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import time
+import sys
+
+nx = 41
+dx = 2/(nx -1)
+nt = 100
+dt = 0.025
+c = 1
+
+u = np.ones(nx)
+u[int(0.5/dx):int(1/dx+1)] = 2
+print(u)
+
+plt.plot(np.linspace(0, 2, nx), u)
+plt.show()
+
+un = np.ones(nx)
+
+for n in range(nt):
+    un = u.copy()
+    for i in range(1, nx):
+        u[i] = un[i] - c* (dt/dx)* (un[i]- un[i-1])
+    if n % 10 == 0:
+        plt.plot(np.linspace(0, 2, nx), u)
+        plt.show()
+
+plt.plot(np.linspace(0, 2, nx), u)
+plt.show()
