37ac7323a685febc79314946528787c32e860947
Felix Steinberger Eriksson <felix.s.eriksson@gmail.com>
2020-04-18 02:54:17 +0200

    lite cleanup

--- 12steps/step1-linearconvection.py
+++ 12steps/step1-linearconvection.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 from matplotlib import pyplot as plt
 import time
@@ -28,3 +29,15 @@ for n in range(nt):
 
 plt.plot(np.linspace(0, 2, nx), u)
 plt.show()
+'''
+#lånar denna för en rekursiv sak i Matematik 1
+def G(n):
+    if n == 0:
+        return 1
+    elif n == 1:
+        return 1
+    else:
+        return 3*G(n-1) + 3*G(n-2)
+
+print(G(30))
+'''
