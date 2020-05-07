50b285cd4f885787d96ee7bdde1fd4035093676e
Felix Steinberger Eriksson <felix.s.eriksson@gmail.com>
2020-05-07 03:39:15 +0200

    some visualisation etc

--- model/boundaryconditions.py
+++ model/boundaryconditions.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from obstacles import *
+
 def build_up_b(rho, dt, dx, dy, u, v):
     b = np.zeros_like(u)
     b[1:-1, 1:-1] = (rho * (1 / dt * ((u[1:-1, 2:] - u[1:-1, 0:-2]) / (2 * dx) +
@@ -27,7 +29,7 @@ def build_up_b(rho, dt, dx, dy, u, v):
     
     return b
 
-def pressure_poisson_periodic(p, dx, dy, nit,b):
+def pressure_poisson_periodic(p, dx, dy, nit, b, shape=None):
     pn = np.empty_like(p)
     
     for _ in range(nit):
@@ -50,10 +52,11 @@ def pressure_poisson_periodic(p, dx, dy, nit,b):
                       dx**2 * dy**2 / (2 * (dx**2 + dy**2)) * b[1:-1, 0])
         
         # Wall boundary conditions, pressure
-        p[-1, :] =p[-2, :]  # dp/dy = 0 at y = top
-        p[0, :] = p[1, :]  # dp/dy = 0 at y = 0
+        p = pressureatwall(p)
 
-        
+        # pressure boundary conditions for obstacle
+        # obstacledict[shape]()
+        p = pressureatfixedsquare(p)
     
     return p
 
@@ -64,3 +67,44 @@ def noslipatwall(u,v):
     v[-1, :] = 0
     return u, v
 
+def pressureatwall(p):
+    p[-1, :] = p[-2, :]  # dp/dy = 0 at y = top
+    p[0, :] = p[1, :]  # dp/dy = 0 at y = 0
+    return p
+
+
+# no-slip function for obstacles
+def noslipatobstacle(shape):
+    return 0
+
+def noslipatsquare(u, v, sidelength=1.4, centerx=2.7, centery=2):
+    # default: hörn i (2, 1.3), (3.4, 1.3), (2, 2.7) och (3.4, 2.7)
+    # (anpassat för medium i 4 x 16 ruta)
+    return 0
+
+def noslipatfixedsquare(u, v):
+    # this function has no variable input and works ONLY for
+    # a 4 x 16 box with nx = ny = 101
+    for indexy in range(32, 69):
+        for indexx in range(12, 23):
+            u[indexy, indexx] = 0
+            v[indexy, indexx] = 0
+    return u, v
+
+# pressure bc function for obstacles, called in pressure_poisson_periodic
+def pressureatobstacle(shape):
+    return 0
+
+def pressureatfixedsquare(p):
+    # this function has no variable input and works ONLY for
+    # a 4 x 16 box with nx = ny = 101
+    for indexy in range(32, 69):
+        for indexx in range(12, 23):
+            p[indexy, indexx] = 0
+    for indexx in range(12, 23):
+        p[32, indexx] = p[31, indexx]
+        p[68, indexx] = p[69, indexx]
+    for indexy in range(32, 69):
+        p[indexy, 12] = p[indexy, 11]
+        p[indexy, 22] = p[indexy, 23]
+    return p
--- model/main.py
+++ model/main.py
@@ -1,20 +1,23 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.animation import FuncAnimation
+from datetime import datetime
+from os import mkdir
 
 from grid import generategrid
-from visualisation import makebox, visualisevelocityvectors
-from boundaryconditions import build_up_b, pressure_poisson_periodic, noslipatwall
+from visualisation import makebox, visualisevelocityvectors, visualisefixedsquare, visualisepressure, savesnap, initdirectory
+from boundaryconditions import build_up_b, pressure_poisson_periodic, noslipatwall, noslipatfixedsquare
 
 
 
 # variable declarations
-nx = 41
-ny = 41
-nt = 500
+nx = 101
+ny = 101
+nt = 50000
 nit = 50 
 c = 1
-displayfreq = 50
+displayfreq = 10
 
 # make grid
 gridsizex = 16
@@ -24,9 +27,9 @@ dx, dy, X, Y = generategrid(gridsizex, gridsizey, nx, ny)
 
 ##physical variables
 rho = 1
-nu = .1
-F = 1
-dt = .01
+nu = 0.5
+F = 0
+dt = 0.001
 
 #initial conditions
 u = np.zeros((ny, nx))
@@ -48,7 +51,7 @@ for timestep in range(nt):
     vn = v.copy()
 
     b = build_up_b(rho, dt, dx, dy, u, v)
-    p = pressure_poisson_periodic(p, dx, dy, nit, b)
+    p = pressure_poisson_periodic(p, dx, dy, nit, b, shape=None)
 
     u[1:-1, 1:-1] = (un[1:-1, 1:-1] -
                     un[1:-1, 1:-1] * dt / dx * 
@@ -74,66 +77,90 @@ for timestep in range(nt):
                     (vn[1:-1, 2:] - 2 * vn[1:-1, 1:-1] + vn[1:-1, 0:-2]) +
                     dt / dy**2 * 
                     (vn[2:, 1:-1] - 2 * vn[1:-1, 1:-1] + vn[0:-2, 1:-1])))
+    
+    # Inlet Condition
+    u[1:-1, 0] = 1
 
-    # Periodic BC u @ x = right
-    u[1:-1, -1] = (un[1:-1, -1] - un[1:-1, -1] * dt / dx * 
-                (un[1:-1, -1] - un[1:-1, -2]) -
-                vn[1:-1, -1] * dt / dy * 
-                (un[1:-1, -1] - un[0:-2, -1]) -
-                dt / (2 * rho * dx) *
-                (p[1:-1, 0] - p[1:-1, -2]) + 
-                nu * (dt / dx**2 * 
-                (un[1:-1, 0] - 2 * un[1:-1,-1] + un[1:-1, -2]) +
-                dt / dy**2 * 
-                (un[2:, -1] - 2 * un[1:-1, -1] + un[0:-2, -1])) + F * dt)
+    # # Periodic BC u @ x = right
+    # u[1:-1, -1] = (un[1:-1, -1] - un[1:-1, -1] * dt / dx * 
+    #             (un[1:-1, -1] - un[1:-1, -2]) -
+    #             vn[1:-1, -1] * dt / dy * 
+    #             (un[1:-1, -1] - un[0:-2, -1]) -
+    #             dt / (2 * rho * dx) *
+    #             (p[1:-1, 0] - p[1:-1, -2]) + 
+    #             nu * (dt / dx**2 * 
+    #             (un[1:-1, 0] - 2 * un[1:-1,-1] + un[1:-1, -2]) +
+    #             dt / dy**2 * 
+    #             (un[2:, -1] - 2 * un[1:-1, -1] + un[0:-2, -1])) + F * dt)
 
-    # Periodic BC u @ x = 0
-    u[1:-1, 0] = (un[1:-1, 0] - un[1:-1, 0] * dt / dx *
-                (un[1:-1, 0] - un[1:-1, -1]) -
-                vn[1:-1, 0] * dt / dy * 
-                (un[1:-1, 0] - un[0:-2, 0]) - 
-                dt / (2 * rho * dx) * 
-                (p[1:-1, 1] - p[1:-1, -1]) + 
-                nu * (dt / dx**2 * 
-                (un[1:-1, 1] - 2 * un[1:-1, 0] + un[1:-1, -1]) +
-                dt / dy**2 *
-                (un[2:, 0] - 2 * un[1:-1, 0] + un[0:-2, 0])) + F * dt)
+    # # Periodic BC u @ x = 0
+    # u[1:-1, 0] = (un[1:-1, 0] - un[1:-1, 0] * dt / dx *
+    #             (un[1:-1, 0] - un[1:-1, -1]) -
+    #             vn[1:-1, 0] * dt / dy * 
+    #             (un[1:-1, 0] - un[0:-2, 0]) - 
+    #             dt / (2 * rho * dx) * 
+    #             (p[1:-1, 1] - p[1:-1, -1]) + 
+    #             nu * (dt / dx**2 * 
+    #             (un[1:-1, 1] - 2 * un[1:-1, 0] + un[1:-1, -1]) +
+    #             dt / dy**2 *
+    #             (un[2:, 0] - 2 * un[1:-1, 0] + un[0:-2, 0])) + F * dt)
 
-    # Periodic BC v @ x = right
-    v[1:-1, -1] = (vn[1:-1, -1] - un[1:-1, -1] * dt / dx *
-                (vn[1:-1, -1] - vn[1:-1, -2]) - 
-                vn[1:-1, -1] * dt / dy *
-                (vn[1:-1, -1] - vn[0:-2, -1]) -
-                dt / (2 * rho * dy) * 
-                (p[2:, -1] - p[0:-2, -1]) +
-                nu * (dt / dx**2 *
-                (vn[1:-1, 0] - 2 * vn[1:-1, -1] + vn[1:-1, -2]) +
-                dt / dy**2 *
-                (vn[2:, -1] - 2 * vn[1:-1, -1] + vn[0:-2, -1])))
+    # # Periodic BC v @ x = right
+    # v[1:-1, -1] = (vn[1:-1, -1] - un[1:-1, -1] * dt / dx *
+    #             (vn[1:-1, -1] - vn[1:-1, -2]) - 
+    #             vn[1:-1, -1] * dt / dy *
+    #             (vn[1:-1, -1] - vn[0:-2, -1]) -
+    #             dt / (2 * rho * dy) * 
+    #             (p[2:, -1] - p[0:-2, -1]) +
+    #             nu * (dt / dx**2 *
+    #             (vn[1:-1, 0] - 2 * vn[1:-1, -1] + vn[1:-1, -2]) +
+    #             dt / dy**2 *
+    #             (vn[2:, -1] - 2 * vn[1:-1, -1] + vn[0:-2, -1])))
 
-    # Periodic BC v @ x = 0
-    v[1:-1, 0] = (vn[1:-1, 0] - un[1:-1, 0] * dt / dx *
-                (vn[1:-1, 0] - vn[1:-1, -1]) -
-                vn[1:-1, 0] * dt / dy *
-                (vn[1:-1, 0] - vn[0:-2, 0]) -
-                dt / (2 * rho * dy) * 
-                (p[2:, 0] - p[0:-2, 0]) +
-                nu * (dt / dx**2 * 
-                (vn[1:-1, 1] - 2 * vn[1:-1, 0] + vn[1:-1, -1]) +
-                dt / dy**2 * 
-                (vn[2:, 0] - 2 * vn[1:-1, 0] + vn[0:-2, 0])))
-
+    # # Periodic BC v @ x = 0
+    # v[1:-1, 0] = (vn[1:-1, 0] - un[1:-1, 0] * dt / dx *
+    #             (vn[1:-1, 0] - vn[1:-1, -1]) -
+    #             vn[1:-1, 0] * dt / dy *
+    #             (vn[1:-1, 0] - vn[0:-2, 0]) -
+    #             dt / (2 * rho * dy) * 
+    #             (p[2:, 0] - p[0:-2, 0]) +
+    #             nu * (dt / dx**2 * 
+    #             (vn[1:-1, 1] - 2 * vn[1:-1, 0] + vn[1:-1, -1]) +
+    #             dt / dy**2 * 
+    #             (vn[2:, 0] - 2 * vn[1:-1, 0] + vn[0:-2, 0])))
+    # v[1:-1, 0] = 0
 
     # Wall BC: u,v = 0 @ y = bottom, top
     u, v = noslipatwall(u, v)
+    u, v = noslipatfixedsquare(u, v)
+    # u[25:75, 13:26] = 0
+    # v[25:75, 13:26] = 0
+
+    # Obstacle BC: u,v = 0 @ obstacle borders
 
     print(stepcount)
+    if stepcount == 0:
+        now = datetime.now()
+        dtstring = now.strftime("%d%m%y-%H%M%S")
+        dirstring = "C:/Users/felix/Documents/GitHub/gymnasiearbete/model/images/" + str(dtstring)
+        mkdir(dirstring)
 
     if stepcount % displayfreq == 0:
 
         fig = plt.figure(figsize = (11,7), dpi=100)
+        plt.subplot(211)
         makebox(gridsizex, gridsizey)
-        visualisevelocityvectors(X, Y, u, v, declutter=False)
-        plt.show()
+        visualisefixedsquare()
+        visualisevelocityvectors(X, Y, u, v, declutter=True)
+        plt.axis("scaled")
+        plt.subplot(212)
+        makebox(gridsizex, gridsizey)
+        visualisefixedsquare()
+        visualisepressure(X, Y, p)
+        plt.axis("scaled")
+
+        savesnap(dtstring, stepcount)
+        plt.close()
+        #plt.show()
 
     stepcount += 1
